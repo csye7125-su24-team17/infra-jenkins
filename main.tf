480efcd6915b72e5d2fd0a42020309c8dc14a522
Eashan Roy <roy.ea@northeastern.edu>
2024-05-28 19:56:55 -0400

    chore: terraform fmt check

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
 
   tags = {
-   Name = "Infra VPC"
- }
+    Name = "Infra VPC"
+  }
 }
 resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.main.id
